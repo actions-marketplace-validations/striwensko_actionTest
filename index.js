0dbbc85cc7051d4371a2c2ef03c88646d66b2965
Juan Carlos <juancarlos.arias@gmail.com>
2021-05-04 12:50:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ try {
   console.log(`The event payload is: ${payload}`);
   console.log('/home/runner/work/react-setup/react-setup/src/App.js');
   console.log("Error:   5:9  error  'time' is assigned a value but never used. eslint-disable no-unused-vars");
+  core.warning('myInput was not set');
+  core.info('Output to the actions build log')
 
 } catch (error) {
   core.setFailed(error.message);
