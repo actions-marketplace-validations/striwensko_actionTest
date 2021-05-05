a92994b61fb6ef507636c876ed422f7a50b80bad
Juan Carlos <juancarlos.arias@gmail.com>
2021-05-04 22:24:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ try {
   console.log('/home/runner/work/react-setup/react-setup/src/App.js');
   console.log("Error:   5:9  error  'time' is assigned a value but never used. eslint-disable no-unused-vars");
   core.warning('myInput was not set');
-  core.warning(JSON.stringify(process.env.GITHUB_REPOSITORY));
+  core.warning(JSON.stringify(process.env));
 
 } catch (error) {
   core.setFailed(error.message);
